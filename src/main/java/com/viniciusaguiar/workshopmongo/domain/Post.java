780c8a43e50ca176d84bdbef520156b2b14c209d
Vinicius <vini1068@gmail.com>
2023-11-28 22:04:55 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/viniciusaguiar/workshopmongo/domain/Post.java
+++ src/main/java/com/viniciusaguiar/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.viniciusaguiar.workshopmongo.domain;
 
 import com.viniciusaguiar.workshopmongo.dto.AuthorDTO;
+import com.viniciusaguiar.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 @Document
 public class Post implements Serializable {
@@ -16,6 +19,8 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -66,6 +71,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
